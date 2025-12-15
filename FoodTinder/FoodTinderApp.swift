d730e344c8f32b22091797cedefd6b3ee9b211f6
yizhe <liaoyizhe75@gmail.com>
2025-12-15 10:49:27 +0800

    Initial Commit

---
+++ FoodTinder/FoodTinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodTinderApp.swift
+//  FoodTinder
+//
+//  Created by 廖邑哲 on 2025/12/15.
+//
+
+import SwiftUI
+
+@main
+struct FoodTinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FoodTinderTests/FoodTinderTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoodTinderTests.swift
+//  FoodTinderTests
+//
+//  Created by 廖邑哲 on 2025/12/15.
+//
+
+import Testing
+@testable import FoodTinder
+
+struct FoodTinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
